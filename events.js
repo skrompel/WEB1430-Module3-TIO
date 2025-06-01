65ccb7100f0c0982b5f46179d6719993edf04406
skrompel <saharakrompel@mail.weber.edu>
2025-06-01 15:33:23 -0600

    Add starter files

---
+++ events.js
@@ -0,0 +1,38 @@
+var dayEvent = new Array();
+
+dayEvent[1] = "";
+dayEvent[2] = "<br /><a href='#'>Tone & Define</a><br />6AM-7AM";
+dayEvent[3] = "<br /><a href='#'>Body Works 2.0</a><br />7PM-8PM";
+dayEvent[4] = "<br /><a href='#'>Spin City Favorites</a><br />6AM-7AM";
+dayEvent[5] = "<br /><a href='#'>Senior Swim</a><br />11AM-1PM";
+dayEvent[6] = "<br /><a href='#'>Light Weights</a><br />7PM-8PM";
+dayEvent[7] = "";
+
+
+dayEvent[8] = "<br /><a href='#'>Senior Swim</a><br />11AM-1PM";
+dayEvent[9] = "<br /><a href='#'>Body Works 2.0</a><br />7PM-8PM";
+dayEvent[10] = "<br /><a href='#'>Body Works 2.0</a><br />7PM-8PM";
+dayEvent[11] = "<br /><a href='#'>Body Works 2.0</a><br />7PM-8PM";
+dayEvent[12] = "<br /><a href='#'>Light Weights</a><br />7PM-8PM";
+dayEvent[13] = "";
+dayEvent[14] = "<br /><a href='#'>Kids Gym</a><br />4PM-5PM";
+
+dayEvent[15] = "<br /><a href='#'>Body Works 2.0</a><br />7PM-8PM";
+dayEvent[16] = "<br /><a href='#'>Spin City Favorites</a><br />6AM-7AM";
+dayEvent[17] = "<br /><a href='#'>Senior Swim</a><br />11AM-1PM";
+dayEvent[18] = "<br /><a href='#'>Body Works 2.0</a><br />7PM-8PM";
+dayEvent[19] = "<br /><a href='#'>Classic Weightlifting</a><br />11AM-NOON";
+dayEvent[20] = "<br /><a href='#'>Tone & Define</a><br />6AM-7AM";
+dayEvent[21] = "";
+
+dayEvent[22] = "<br /><a href='#'>Body Works 2.0</a><br />7PM-8PM";
+dayEvent[23] = "<br /><a href='#'>Body Works 2.0</a><br />7PM-8PM";
+dayEvent[24] = "<br /><a href='#'>Kids Gym</a><br />4PM-5PM";
+dayEvent[25] = "<br /><a href='#'>Spin City Favorites</a><br />6AM-7AM";
+dayEvent[26] = "<br /><a href='#'>Senior Swim</a><br />11AM-1PM";
+dayEvent[27] = "";
+dayEvent[28] = "<br /><a href='#'>Senior Swim</a><br />11AM-1PM";
+
+dayEvent[29] = "<br /><a href='#'>Spin City Favorites</a><br />6AM-7AM";
+dayEvent[30] = "";
+dayEvent[31] = "<br /><a href='#'>Light Weights</a><br />7PM-8PM";
